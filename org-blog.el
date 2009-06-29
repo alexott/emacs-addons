5c1199b3550be328844f9d6e46101ea480de4dd1
Alex Ott <alexott@gmail.com>
2009-06-29 11:42:31 +0200

    start development of blog exporter (Atom) for .org files

---
+++ org-blog.el
@@ -0,0 +1,17 @@
+;;; org-blog.el ---
+
+;; Copyright (C) Alex Ott
+;;
+;; Author: Alex Ott <alexott@gmail.com>
+;; Keywords:
+;; Requirements:
+;; Status: not intended to be distributed yet
+
+
+(require 'org-export-generic)
+
+
+(provide 'org-blog)
+
+
+;;; org-blog.el ends here
